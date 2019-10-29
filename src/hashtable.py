92e6741fdbd0469b72794509dd0299b7bb48f573
mattshardman <mattshardman@gmail.com>
2019-10-29 14:52:20 +0000

    update hash function

--- src/hashtable.py
+++ src/hashtable.py
@@ -8,23 +8,19 @@ class LinkedPair:
         self.next = None
 
 class HashTable:
-    '''
-    A hash table that with `capacity` buckets
-    that accepts string keys
-    '''
     def __init__(self, capacity):
+        self.count = 0
+        self.initial_capacity = capacity
         self.capacity = capacity  # Number of buckets in the hash table
         self.storage = [None] * capacity
-
 
     def _hash(self, key):
         '''
         Hash an arbitrary key and return an integer.
-
+        
         You may replace the Python hash with DJB2 as a stretch goal.
         '''
-        return hash(key)
-
+        return hash(key) % self.capacity
 
     def _hash_djb2(self, key):
         '''
@@ -34,59 +30,7 @@ class HashTable:
         '''
         pass
 
-
-    def _hash_mod(self, key):
-        '''
-        Take an arbitrary key and return a valid integer index
-        within the storage capacity of the hash table.
-        '''
-        return self._hash(key) % self.capacity
-
-
-    def insert(self, key, value):
-        '''
-        Store the value with the given key.
-
-        Hash collisions should be handled with Linked List Chaining.
-
-        Fill this in.
-        '''
-        pass
-
-
-
-    def remove(self, key):
-        '''
-        Remove the value stored with the given key.
-
-        Print a warning if the key is not found.
-
-        Fill this in.
-        '''
-        pass
-
-
-    def retrieve(self, key):
-        '''
-        Retrieve the value stored with the given key.
-
-        Returns None if the key is not found.
-
-        Fill this in.
-        '''
-        pass
-
-
-    def resize(self):
-        '''
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
-
-        Fill this in.
-        '''
-        pass
-
-
+    
 
 if __name__ == "__main__":
     ht = HashTable(2)
@@ -107,8 +51,15 @@ if __name__ == "__main__":
     ht.resize()
     new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    print(old_capacity, new_capacity)
+    ht.remove("line_1")
+    ht.remove("line_2")
+    ht.remove("line_3")
 
+    old_capacity = len(ht.storage)
+    ht.resize()
+    new_capacity = len(ht.storage)
+    print(old_capacity, new_capacity)
     # Test if data intact after resizing
     print(ht.retrieve("line_1"))
     print(ht.retrieve("line_2"))
